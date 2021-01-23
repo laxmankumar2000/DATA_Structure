3e1ef274b1f53ca23daef0e416e22dc8068c29bd
laxmankumar2000 <laxmankumarvashisth@gmail.com>
2021-01-23 20:58:47 +0530

    c

--- src/CombineProgram/CheckAnagram.java
+++ src/CombineProgram/CheckAnagram.java
@@ -10,31 +10,33 @@ public class CheckAnagram {
         str1 = str1.replaceAll("\\s" , "");            //Remove space from 1st String
         str2 = str2.replaceAll("\\s" , "");            //Remove space from 2nd String
 
-        boolean status = true;                                      //Take a bolean variable
-        if(str1.length() != str2.length())                               //Length Compare
+        boolean status = true;                                          //Take a bolean variable
+        try {
+
+            if (str1.length() != str2.length())                               //Length Compare
+            {
+                status = false;
+            } else {
+                char ch1[] = str1.toLowerCase().toCharArray();           //Convert String into Char and Store in Char Array
+                char ch2[] = str2.toLowerCase().toCharArray();           //Convert String into Char and Store in Char Array
+
+                Arrays.sort(ch1);                                      //Sort char Array1
+                Arrays.sort(ch2);                                      //Sort char Array2
+
+                status = Arrays.equals(ch1, ch2);                       //equale Char1 and Char2 ,, and store boolean value in boolean varialbe
+            }
+
+            if (status) {
+                System.out.println(str1 + " and " + str2 + " is ANAGRAM");  // print String is Anagram
+            } else {
+                System.out.println(str1 + " and " + str2 + " is NOT ANAGRAM"); //print String is not Anagram
+            }
+        }
+        catch (Exception e)
         {
-            status = false;
+            e.printStackTrace();
         }
 
-        else
-        {
-            char ch1[] = str1.toLowerCase().toCharArray();           //Convert String into Char and Store in Char Array
-            char ch2[] = str2.toLowerCase().toCharArray();           //Convert String into Char and Store in Char Array
-
-            Arrays.sort(ch1);                                      //Sort char Array1
-            Arrays.sort(ch2);                                      //Sort char Array2
-
-            status = Arrays.equals(ch1,ch2);                       //equale Char1 and Char2 ,, and store boolean value in boolean varialbe
-        }
-
-        if(status)
-        {
-            System.out.println(str1 + " and " + str2 + "is ANAGRAM");  // print String is Anagram
-        }
-        else
-        {
-            System.out.println(str1 + " and " + str2 + " is NOT ANAGRAM"); //print String is not Anagram
-        }
 
     }
 
